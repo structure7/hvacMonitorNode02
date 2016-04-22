2c4ca6758b3e8e8221a8f51ca5a5581195b7dcf6
structure7 <kemperm@usa.net>
2016-04-22 16:28:43 -0700

    Added heartbeat

--- hvacMonitorNode02.ino
+++ hvacMonitorNode02.ino
@@ -5,26 +5,31 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <TimeLib.h>
-#define ONE_WIRE_BUS 2
+#define ONE_WIRE_BUS 0
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
-DeviceAddress ds18b20kk = { 0x28, 0xEE, 0x9D, 0xEF, 0x00, 0x16, 0x02, 0x56 }; // Bare sensor
+DeviceAddress ds18b20kk = { 0x28, 0xEE, 0x9D, 0xEF, 0x00, 0x16, 0x02, 0x56 }; // KK
 
-char auth[] = "fromBlynkApp";
+char auth[] = "fromBlynkApp *04";
 
 SimpleTimer timer;
+
+WidgetLED led1(V10);
 
 void setup()
 {
   Serial.begin(9600);
   Blynk.begin(auth, "ssid", "pw");
 
+  WiFi.softAPdisconnect(true); // Per https://github.com/esp8266/Arduino/issues/676 this turns off AP
+
   sensors.begin();
   sensors.setResolution(ds18b20kk, 10);
 
   timer.setInterval(2000L, sendTemps); // Temperature sensor polling interval
+  timer.setInterval(5000L, sendHeartbeat); // Temperature sensor polling interval
 }
 
 void sendTemps()
@@ -41,6 +46,12 @@ void sendTemps()
   {
     Blynk.virtualWrite(4, "ERR");
   }
+  led1.off();
+}
+
+void sendHeartbeat()
+{
+  led1.on();
 }
 
 void loop()
